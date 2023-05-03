1df2afa6b8c58a66771f0f8767d090275fba3bd4
RUVNE <dian.galleon@gmail.com>
2023-05-04 03:23:08 +0800

    fix period php model

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Period extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'start_date',
+        'end_date',
+        'is_active',
+    ];
+    
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('is_active', false);
+    }
 }
